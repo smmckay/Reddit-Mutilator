36f07e365ffa2e993fab20b7ca76ac41b714cc73
Steve McKay <smmckay@broncs.utpa.edu>
2010-09-09 21:04:53 -0500

    prefs and options in place for hide share|save|hide|report|create|submit

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,6 +1,12 @@
 pref("extensions.redditmutilator.blocknsfw", true);
 pref("extensions.redditmutilator.blockdomains", "");
 pref("extensions.redditmutilator.blockusers", "");
+pref("extensions.redditmutilator.hideshare", false);
+pref("extensions.redditmutilator.hidesave", false);
+pref("extensions.redditmutilator.hidehide", false);
+pref("extensions.redditmutilator.hidereport", false);
+pref("extensions.redditmutilator.hidecreatebox", false);
+pref("extensions.redditmutilator.hidesubmitbox", false);
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.{a00b4d87-e032-49c0-ba21-abbe079b51ae}.description", "chrome://redditmutilator/locale/overlay.properties");
